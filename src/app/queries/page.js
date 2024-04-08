3ae4efaee8ae3ad4c8e2f3890486c15cf01ee383
sethbehar <sethbehar@gmail.com>
2024-04-08 19:44:43 -0400

    query page

--- src/app/queries/page.js
+++ src/app/queries/page.js
@@ -8,10 +8,20 @@ export default function Queries() {
             <Navbar isUserAuthenticated={true} activeLink="dashboard" />
             <div className={styles.mainDiv}>
                 <div className={styles.create}>
-                    
+                    <div className={styles.queryHeader}>
+                        Select a query
+                    </div>
+                    <div className={styles.gridLayout}>
+                        <span className={styles.queries}>Query 1</span>
+                        <span className={styles.queries}>Query 2</span>
+                        <span className={styles.queries}>Query 3</span>
+                        <span className={styles.queries}>Query 4</span>
+                        <span className={styles.queries}>Query 5</span>
+                        <span className={styles.queries}>Query 6</span>
+                    </div>
                 </div>
                 <div className={styles.results}>
-                
+
                 </div>
             </div>
         </>
