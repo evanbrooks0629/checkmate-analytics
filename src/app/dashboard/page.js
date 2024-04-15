908a5c52fe601cec0ce6185dc3743972a8a8b058
jakemoss127 <jacobmoss@ufl.edu>
2024-04-15 18:38:03 -0400

    Fixed players bug

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -63,7 +63,7 @@ function PlayerList() {
           style={{marginTop: "5px" }}
         ></div>
         {players.map((player) => (
-          <li key={player.PLAYERNAME}>
+          <li key={player.PLAYERID}>
             {player.PLAYERNAME}
           </li>
         ))}
@@ -115,80 +115,80 @@ export default function Dashboard() {
   const games = GamesList();
 
 return (
-    <>
-        <Navbar isUserAuthenticated={true} activeLink="dashboard" />
-        <div className={styles.Home}>
-            <div className={styles.Content}>
-                <div className={styles.TopGrid}>
-                    <div className={styles.QueryRow}>
-                        <h2 className={styles.Title}>View a Query</h2>
-                        <button
-                            className={styles.btn_primary}
-                            onClick={() => (window.location.href = "/queries")}
-                        >
-                            See Queries
-                        </button>
-                    </div>
-                    <p className={styles.p}>
-                        Effortlessly craft queries to find the exact data you are looking
-                        for
-                    </p>
-                </div>
-                <div className={styles.TopGrid}>
-                    <div className={styles.QueryRow}>
-                        <h2 className={styles.Title}>View Charts</h2>
-                        <button
-                            className={styles.btn_primary}
-                            onClick={() => (window.location.href = "/charts")}
-                        >
-                            See Statistics
-                        </button>
-                    </div>
-                    <p className={styles.p}>
-                        View our free charts which show different data over time
-                    </p>
-                </div>
-                <div className={styles.BottomGrid}>
-                    <h3 className={styles.BottomTitle}>Recent Grandmaster Games</h3>
-                    <div className={styles.RecentBox}>{games}</div>
-                </div>
-                <div className={styles.BottomGrid}>
-                    <h3 className={styles.BottomTitle}>Find a Player</h3>
-                    <div className={styles.SearchRow}>
-                        <input
-                            className={styles.SearchBar}
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)} // Update name state on input change
-                            placeholder="Enter player name"
-                        ></input>
-                        <button className={styles.btn_primary} onClick={fetchPlayerStats}>
-                            Search
-                        </button>
-                    </div>
-                    <div className={styles.SearchResults}>
-                        {error && <div className={styles.errorMsg}>{error}</div>}
-                        {player && (
-                            <>
-                                <div className={styles.playerName}>
-                                    <p>
-                                        {player.REALNAME !== "N/A"
-                                            ? player.REALNAME
-                                            : player.PLAYERNAME}
-                                    </p>
-                                </div>
-                                <div className={styles.border} />
-                                <div className={styles.stats}>
-                                    <p>ELO: {player.ELO}</p>
-                                    <p>Player ID: {player.PLAYERID}</p>
-                                </div>
-                            </>
-                        )}
-                        {!player && players}
-                    </div>
-                </div>
-            </div>
+  <>
+    <Navbar isUserAuthenticated={true} activeLink="dashboard" />
+    <div className={styles.Home}>
+      <div className={styles.Content}>
+        <div className={styles.TopGrid}>
+          <div className={styles.QueryRow}>
+            <h2 className={styles.Title}>View a Query</h2>
+            <button
+              className={styles.btn_primary}
+              onClick={() => (window.location.href = "/queries")}
+            >
+              See Queries
+            </button>
+          </div>
+          <p className={styles.p}>
+            Effortlessly craft queries to find the exact data you are looking
+            for
+          </p>
         </div>
-    </>
+        <div className={styles.TopGrid}>
+          <div className={styles.QueryRow}>
+            <h2 className={styles.Title}>View Charts</h2>
+            <button
+              className={styles.btn_primary}
+              onClick={() => (window.location.href = "/charts")}
+            >
+              See Statistics
+            </button>
+          </div>
+          <p className={styles.p}>
+            View our free charts which show different data over time
+          </p>
+        </div>
+        <div className={styles.BottomGrid}>
+          <h3 className={styles.BottomTitle}>Recent Grandmaster Games</h3>
+          <div className={styles.RecentBox}>{games}</div>
+        </div>
+        <div className={styles.BottomGrid}>
+          <h3 className={styles.BottomTitle}>Find a Player</h3>
+          <div className={styles.SearchRow}>
+            <input
+              className={styles.SearchBar}
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)} // Update name state on input change
+              placeholder="Enter player name"
+            ></input>
+            <button className={styles.btn_primary} onClick={fetchPlayerStats}>
+              Search
+            </button>
+          </div>
+          <div className={styles.SearchResults}>
+            {error && <div className={styles.errorMsg}>{error}</div>}
+            {player && (
+              <>
+                <div className={styles.playerName}>
+                  <p>
+                    {player.REALNAME !== "N/A"
+                      ? player.REALNAME
+                      : player.PLAYERNAME}
+                  </p>
+                </div>
+                <div className={styles.border} />
+                <div className={styles.stats}>
+                  <p>ELO: {player.ELO}</p>
+                  <p>Player ID: {player.PLAYERID}</p>
+                </div>
+              </>
+            )}
+            {!player && <PlayerList />}
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 );
 }
